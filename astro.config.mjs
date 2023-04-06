aff09922f12407c877f6396d437b5165d61daa65
Peter Klooster <crashkonijn@gmail.com>
2023-04-06 13:14:14 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import tailwind from '@astrojs/tailwind'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://crashkonijn.com',
-  base: '/',
   integrations: [compress(), mdx(), tailwind()],
 })
