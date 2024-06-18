2a3153c340554000223965b3614e0ccd0a44924e
Peter Klooster <crashkonijn@gmail.com>
2024-06-18 21:39:54 +0200

    Fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import compress from 'astro-compress';
 import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 
@@ -8,5 +7,5 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://crashkonijn.com',
-  integrations: [compress(), mdx(), tailwind(), react()]
+  integrations: [mdx(), tailwind(), react()]
 });
